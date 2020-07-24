6b07f22079f32cce57046710b749d17c60ed952d
224247 Andryskowski Stanisław <224247@edu.p.lodz.pl>
2020-07-24 18:03:32 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+//variables from HTML
 const POINTS = document.querySelector('.points');
-
-let counter_of_click = 1;
 let cards = document.querySelectorAll('.card_back');
 const CARDS = [...cards];
 const WAITING_CARDS = [];
 //counters
+let counter_of_click = 1;
 let counter_if_found = 0;
 let counter_timer = 0;
 let winner_time = 1000;
+//array for backgrounds of cards
 let backgrounds_cards = [];
 
+//fuction onload
 window.onload = function () {
     set_interval_for_game();
     shuffleArray(CARDS);
@@ -33,15 +35,15 @@ window.onload = function () {
     setTimeout(function () { CARDS.forEach(CARD => CARD.style.backgroundImage = 'url(img/card.svg)') }, 2000);
 };
 
+//function 'click' for each card
 CARDS.forEach(CARD => CARD.addEventListener('click', function (index) {
     if (WAITING_CARDS.length < 2) {
-        show_card(CARD)
+        move_card(CARD)
     }
     if (WAITING_CARDS.length == 2 && (WAITING_CARDS[0].getAttribute('data-if_active') == 'active' && WAITING_CARDS[1].getAttribute('data-if_active') == 'active') && (WAITING_CARDS[0].style.backgroundImage == WAITING_CARDS[1].style.backgroundImage) && (WAITING_CARDS[0].getAttribute('data-index') != WAITING_CARDS[1].getAttribute('data-index'))) {
         WAITING_CARDS[0].style.opacity = 0;
         WAITING_CARDS[1].style.opacity = 0;
         counter_if_found += 2;
-        console.log(counter_if_found);
         WAITING_CARDS.length = 0;
         if (counter_if_found == 16) {
             you_win();
@@ -49,7 +51,8 @@ CARDS.forEach(CARD => CARD.addEventListener('click', function (index) {
     }
 }))
 
-function show_card(CARD) {
+//function to moving card
+function move_card(CARD) {
     CARD.style.backgroundImage = backgrounds_cards[CARDS.indexOf(CARD)];
     CARD.classList.add('active');
     CARD.setAttribute('data-if_active', 'active');
@@ -63,6 +66,7 @@ function show_card(CARD) {
     }, 2000);
 }
 
+//shuffle array 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -71,21 +75,20 @@ function shuffleArray(array) {
     return array;
 }
 
+//it's necessary to save backgrounds before setting all cards backwards
 function make_array_for_backgrounds(backgrounds_cards) {
-
     for (let i = 0; i < CARDS.length; i++) {
         backgrounds_cards[i] = CARDS[i].style.backgroundImage;
     }
-
     return backgrounds_cards;
 }
 
+//set time for function showTime
 function set_interval_for_game() {
-
     setInterval(showTime, 1000);
-
 }
 
+//setting inscription with counter of seconds
 function showTime() {
     if (counter_timer < winner_time) {
         POINTS.innerHTML = '<h2 class="points">Your score is ' + counter_timer + ' seconds!</h2>';
@@ -96,12 +99,11 @@ function showTime() {
     }
 }
 
+//what if user win
 function you_win() {
     alert("Congratulation! :D You win! Your time is " + counter_timer + " seconds");
     winner_time = counter_timer;
-    console.log(winner_time);
     POINTS.innerHTML = 'Reload page if you want play again. ;)';
 }
 
 
-console.log(winner_time);
